adc2445d13193d5f2cd732fe5fc2ae3b56f3d38e
bhargavvummadi <bhargavdurgaprasad123@gmail.com>
2022-02-11 00:00:38 +0000

    3 commit

---
+++ animals.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+import time
+
+
+def main():
+    i=0
+    l=40
+
+    print("World")
+
+
+if __name__=='__main__':
+    print("Hello")
+    main()

2075f10d557a0572bbe66cffaa5c183522cab0d6
bhargavvummadi <bhargavdurgaprasad123@gmail.com>
2022-02-17 02:56:53 +0000

    1.0 version

---
+++ setup.py
@@ -0,0 +1,15 @@
+#python setup.py build
+#python -m build
+
+from setuptools import setup, find_packages
+
+setup{
+	name='tweetercounter',
+	version='1.0',
+	author='bhargav',
+	author_email='bhargav.vummadi@ou.edu',
+	packages=find_packages(exclude=('test','docs')),
+	setup_requires=['pytest-runner'],
+	test_require=['pytest']
+
+}
---
+++ twittercounter.py
@@ -0,0 +1,9 @@
+
+
+def main():
+	"""This has sample content """
+	pass
+
+if __name__=="__main__":
+	main()
+	print("This is testing of sample code")

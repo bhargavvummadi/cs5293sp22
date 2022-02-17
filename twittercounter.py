50bf760cf9ad5dc43b31d02c058bcef8f9de95a6
bhargavvummadi <bhargavdurgaprasad123@gmail.com>
2022-02-17 20:26:17 +0000

    2/17/20222

--- twittercounter.py
+++ twittercounter.py
@@ -1,14 +1,29 @@
 import argparse
 
-def main(term):
-	"""This has sample content """
-	print(f"The term is: {term}")
-	pass
 
-if __name__=="__main__":
-	parser= argparse.ArgumentParser()
-	parser.add_argument("--term", type=str,required=True,help="The term to look up in the tweets")
-	args =parser.parse_args()
-	print(args)
-	main(args.term)
-	#print("Usage: tweetercounter.py burritos")
+def main(term):
+    """This has sample content"""
+    print(f"The term is: {term}")
+    
+
+def counttweets(filename):
+	"""Count all the unique tweets in a file."""
+	return -1
+
+def searchterms(filename,terms=None)
+	"""Search the file for a list of terms."""
+	return list()
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--term", type=str, required=True, help="The term to look up in the tweets"
+    )
+    args = parser.parse_args()
+    print(args)
+    main(args.term)
+    # print("Usage: tweetercounter.py burritos")

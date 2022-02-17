a2cecdaa9eb853a716fb4764bae821a2e85c5f92
bhargavvummadi <bhargavdurgaprasad123@gmail.com>
2022-02-17 03:09:26 +0000

    updated twittercounter

--- twittercounter.py
+++ twittercounter.py
@@ -1,9 +1,14 @@
+import argparse
 
-
-def main():
+def main(term):
 	"""This has sample content """
+	print(f"The term is: {term}")
 	pass
 
 if __name__=="__main__":
-	main()
-	print("This is testing of sample code")
+	parser= argparse.ArgumentParser()
+	parser.add_argument("--term", type=str,required=True,help="The term to look up in the tweets")
+	args =parser.parse_args()
+	print(args)
+	main(args.term)
+	#print("Usage: tweetercounter.py burritos")
